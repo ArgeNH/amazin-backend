58522623ce2c460d60ca7da82b23ec7f25b8f4a0
Arge Niño <arge.nino@uptc.edu.co>
2022-05-04 09:15:09 -0500

    fix get user, and create update user

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -5,7 +5,7 @@ import User from '../models/User';
 export const getUser = async (req: Request, res: Response) => {
     const { email } = req.params;
     try {
-        const user = await User.findOne({ email }, { password: 0, __v: 0 });
+        const user = await User.findOne({ email }, { password: 0, __v: 0, oldPassword: 0 });
         if (!user) return res.status(400).json({
             success: false,
             message: 'The user is not registered'
@@ -26,7 +26,7 @@ export const getUser = async (req: Request, res: Response) => {
 
 export const getUsers = async (_req: Request, res: Response) => {
     try {
-        const users = await User.find({}, { password: 0, __v: 0 });
+        const users = await User.find({}, { password: 0, __v: 0, oldPassword: 0 });
         if (!users) return res.status(400).json({
             success: false,
             message: 'Users not found'
@@ -45,7 +45,29 @@ export const getUsers = async (_req: Request, res: Response) => {
     }
 }
 
-export const updateUser = async (_req: Request, _res: Response) => { }
+export const updateUser = async (req: Request, res: Response) => {
+    const { email } = req.params;
+    const { name, lastName } = req.body;
+    try {
+        const user = await User.findOneAndUpdate({ email }, { name, lastName }, { new: true });
+        if (!user) return res.status(400).json({
+            success: false,
+            message: 'The user is not registered'
+        });
+        return res.status(200).json({
+            success: true,
+            message: 'User updated successfully',
+            user
+        });
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message: 'Something went wrong',
+            error: error.message
+        });
+    }
+
+}
 
 export const deleteUser = async (req: Request, res: Response) => {
     const { email } = req.params;
